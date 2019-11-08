91774118e3b0a41056765260a1e6c034c6c65719
ManuelNvro <manosnavarro@gmail.com>
2019-11-08 17:26:58 -0500

    Got the 14 bus model to work

--- DymolaPerformance.py
+++ DymolaPerformance.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[2]:
-
-
 import platform
 from dymola.dymola_interface import DymolaInterface
 from dymola.dymola_exception import DymolaException
@@ -11,35 +7,21 @@ from modelicares import SimRes
 import pandas as pd
 import numpy as np
 import os
-
-
-# In[3]:
-
-
-#This is intended to be used in the manuelnvro Dell using Dymola 2020.
-
-
-# In[4]:
-
-
+import time 
 #Setting Dymola Interface
 dymola = DymolaInterface("/opt/dymola-2020-x86_64/bin64/dymola.sh")
 #Setting OpenIPSL library
-dymola.openModel("/home/manuelnvro/dev/Gitted/PythonTesting/OpenIPSL-master/OpenIPSL/package.mo") 
-dymola.openModel("/home/manuelnvro/dev/Gitted/OpenIPSL/ApplicationExamples/IEEE14/package.mo") 
-
-
+dymola.openModel("/home/manuelnvro/dev/Gitted/DymolaPerformance/OpenIPSL-1.5.0/OpenIPSL/package.mo") 
+dymola.openModel("/home/manuelnvro/dev/Gitted/DymolaPerformance/OpenIPSL-1.5.0/ApplicationExamples/IEEE14/package.mo") 
 print("Dymola Simulation Start...")
-
-
-# In[5]:
-
-
 try:
     #Set WorkingDir
-    dymola.cd("/home/manuelnvro/dev/Gitted/PythonTesting/WorkingDir/Dymola")
-    resultPath = "/home/manuelnvro/dev/Gitted/PythonTesting/WorkingDir/Dymola/EXAC1"
+
+    dymola.cd("/home/manuelnvro/dev/Gitted/DymolaPerformance/WorkingDir")
+    resultPath = "/home/manuelnvro/dev/Gitted/DymolaPerformance/WorkingDir"
     #Dymola Simulation
+    dymola.Execute("Advanced.Define.DAEsolver = true")
+    t = time.time()
     result = dymola.simulateModel("IEEE14.IEEE_14_Buses", stopTime=120, numberOfIntervals=0, outputInterval=0.001, tolerance=1e-06, fixedstepsize=0.001, resultFile="IEEE_14_Buses")
     if not result:
         print("Simulation failed. Below is the translation log.")
@@ -49,22 +31,6 @@ try:
     print("Simulation OK")
     #Close Dymola
     dymola.close()
-    #Selecting result file
-    sim = SimRes("/home/manuelnvro/dev/Gitted/PythonTesting/WorkingDir/Dymola/EXAC1.mat")
-    #Selecting Variables
-    variables = ['Time', 'gENROE.delta','gENROE.PELEC', 'eXAC1_1.EFD','gENROE.SPEED', 'GEN1.V', 'LOAD.V', 'GEN2.V', 'FAULT.V' ]
-    df_variables = pd.DataFrame([], columns = variables)
-    for var in variables:
-        df_variables.drop(var, axis = 1, inplace = True)
-        df_variables[var] = np.array(sim[var].values())
-    print("Vars OK")
-    #Changing current directory
-    os.chdir("/home/manuelnvro/dev/Gitted/PythonTesting/ResultData/")
-    #Creating .csv house
-    df_variables.to_csv('dataDymola.csv', index = False)
-    print("Write OK")
+    print(f"time: {time.time() - t}")
 except DymolaException as ex:
     print(("Error: " + str(ex)))
-
-
-# In[ ]:
